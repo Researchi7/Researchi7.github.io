26b89518fcc811995617ab6358fad250e764a129
Chi Xing <chixing@liverpool.ac.uk>
2024-07-09 22:43:31 +0000

    modify to improve mobile experience in resume page

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -6,12 +6,17 @@ const myResume = './Resume1.pdf';
 const ResumePage = () => {
   return (
     <center>
-      <h3>Resume (<a href={myResume} className={styles.underline} download="Resume-KonstantinosGeorgiou.pdf">Download</a>)</h3>
+      <h3>Resume (<a href={myResume} className={styles.underline} download="ChiXing-CV.pdf">Download</a>)</h3>
       <br />
       <div className={styles.pdfContainer}>
         <Document file={myResume}>
           <Page pageIndex={0} scale={1.5} renderMode="svg" />
         </Document>
+      </div>
+      <br />
+      <div className={styles.alert}>
+        <h5>Only download available</h5>
+        <h5>Preview will be available on PC</h5>
       </div>
     </center>
   );
