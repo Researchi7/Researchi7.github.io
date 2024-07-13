2690b9d536351b7a697aae9595f957db9beded74
Chi Xing <chixing@liverpool.ac.uk>
2024-07-13 21:14:34 +0000

    modify readme.md

--- pages/aboutMe.jsx
+++ pages/aboutMe.jsx
@@ -7,13 +7,16 @@ const aboutMePage = () => {
             <h3>A underlying thinker</h3>
             <p>Graduated from University of Liverpool (Hons First Class in Computer Science)</p>
             <p>24 Fall Artificial Intelligence Master in University of Edinburgh</p>
-            <p>My Pathway</p>
+            <p>My Pathway👇</p>
             <ul>
-                <li>hi</li>
-                <li>hi</li>
-                <li>hi</li>
+                <li>UK N8CIR Bede 2024 Summer Internship</li>
+                <li>Stable Contributor of Casbin Community</li>
+                <li>Mentor of 2024 OSPP(Open-Source Promption Plan) and GLCC(GitLink Code Camp)</li>
+                <li>Second Place in 2023 BMW Hackthon</li>
+                <li>Ex-Software Engineer at IFLYTEK</li>
             </ul>
-
+            <p>My Interests🔥</p>
+            NLP
         </div>
     );
 }
