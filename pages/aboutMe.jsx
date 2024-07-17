d79d30f36247d11251734e0f4c120363ed1e8f44
Chi Xing <chixing@liverpool.ac.uk>
2024-07-17 15:52:26 +0000

    add some links for events

--- pages/aboutMe.jsx
+++ pages/aboutMe.jsx
@@ -4,21 +4,27 @@ const aboutMePage = () => {
     return (
         <div className={styles.container}>
             <h1>Chi Xing</h1>
-            <h3>A underlying thinker</h3>
-            <p>Graduated from University of Liverpool (Hons First Class in Computer Science)</p>
-            <p>24 Fall Artificial Intelligence Master in University of Edinburgh</p>
-            <p>My Pathway👇</p>
+            <h3>🪐 A underlying thinker 🤔️</h3>
+            <p>Graduated from Liverpool Uni (Honours First Class in Computer Science)</p>
+            <p>24 Fall Artificial Intelligence Master in Edinburgh Uni</p>
+            <p>⛰️ My Pathway ⛰️</p>
             <ul className={styles.pathwayList}>
-                <li>2024 Summer UK N8CIR Bede Internship</li>
-                <li>2024 Summer Mentor of OSPP(Open-Source Promption Plan) and GLCC(GitLink Code Camp)</li>
-                <li>2024 Stable Contributor of Casbin Community</li>
-                <li>2023 Summer BMW Hackthon - Second Place in Finals</li>
-                <li>2022 Summer Ex-Software Engineer at IFLYTEK</li>
-                <li>* 2021 Summer Undergraduate Research Fellowship in XJTLU</li>
-                <li>* 2020 Summer Ex-Investment Adviser Assistant at EastMoney</li>
+                <li>2024 Summer <a href="https://n8cir.org.uk/" target="_blank"
+                rel="noopener noreferrer" className={styles.underline}>UK N8CIR</a> Bede Internship - Contribute to <a href="https://www.liverpool.ac.uk/computational-biology-facility/" target="_blank"
+              rel="noopener noreferrer" className={styles.underline}>CBF at Liverpool Uni</a></li>
+                <li>2024 Summer Mentor of <a href="https://summer-ospp.ac.cn/" target="_blank"
+                rel="noopener noreferrer" className={styles.underline}>OSPP</a>(Open-Source Promption Plan) and GLCC(GitLink Code Camp)</li>
+                <li>2024 Stable Contributor of <a href="https://github.com/casibase" target="_blank"
+                rel="noopener noreferrer" className={styles.underline}>Casbin</a> Community</li>
+                <li>2023 Summer <a href="https://x.com/ShenyangChina/status/1694602473485320306" target="_blank"
+                rel="noopener noreferrer" className={styles.underline}>BMW Hackthon</a> - Second Place in Finals</li>
+                <li>2022 Summer Ex-Software Engineer at <a href="https://www.iflytek.com/en/" target="_blank"
+                rel="noopener noreferrer" className={styles.underline}>IFLYTEK</a></li>
+                <li>* 2022 Summer Undergraduate Research Fellowship <a href="https://www.xjtlu.edu.cn/en/study/surf/introduction" target="_blank"
+                rel="noopener noreferrer" className={styles.underline}>(SURF)</a> in XJTLU</li>
+                <li>* 2021 Summer Ex-Investment Adviser Assistant at EastMoney</li>
             </ul>
-            <p>My Interests🔥</p>
-            NLP
+            <p>✨ My Interest Points ✨</p>
         </div>
     );
 }
