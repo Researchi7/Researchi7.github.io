0b611289c3e61fa6afb4d8a2d3205ed6d091a01d
Chi Xing <chixing@liverpool.ac.uk>
2024-10-06 10:57:53 +0000

    update resume

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import styles from '../styles/ResumePage.module.css';
-import { pdfjs, Document, Page } from 'react-pdf'
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
+import { pdfjs, Document, Page } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 const myResume = './Resume1.pdf';
 
 const ResumePage = () => {
+  const [numPages, setNumPages] = useState(null);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
     <center>
-      <h3>Resume (<a href={myResume} className={styles.underline} download="ChiXing-CV.pdf">Download</a>)</h3>
+      <h3>
+        Resume (
+        <a href={myResume} className={styles.underline} download="ChiXing-CV.pdf">
+          Download
+        </a>
+        )
+      </h3>
       <br />
       <div className={styles.pdfContainer}>
-        <Document file={myResume}>
-          <Page pageIndex={0} scale={1.5} renderMode="svg" />
+        <Document file={myResume} onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageIndex={index} scale={1.5} renderMode="svg" />
+          ))}
         </Document>
       </div>
       <br />
