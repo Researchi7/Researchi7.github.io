28d6156f269bbf871e96ff33648de2dc64e08f57
Chi Xing <chixing@liverpool.ac.uk>
2024-10-18 16:14:50 +0000

    update posts

--- pages/posts.jsx
+++ pages/posts.jsx
@@ -2,10 +2,9 @@ import PostCard from '../components/PostCard';
 import { getNewsPosts } from './api/news-posts';
 import { getAcademicPosts } from './api/academic-posts';
 import { getLifePosts } from './api/life-posts';
-import { getInterestingTricksPosts } from './api/interesting-tricks-posts';
 import styles from '../styles/PostsPage.module.css';
 
-const PostsPage = ({ news_posts, academic_posts, life_posts, interesting_tricks_posts }) => {
+const PostsPage = ({ news_posts, academic_posts, life_posts }) => {
   return (
     <>
       <h3>Posts</h3>
@@ -34,13 +33,6 @@ const PostsPage = ({ news_posts, academic_posts, life_posts, interesting_tricks_
         ))}
       </div>
       <br/>
-      <center><h4>Interesting Tricks</h4></center>
-      <hr/>
-      <div className={styles.container}>
-        {interesting_tricks_posts.map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}
-      </div>
     </>
   );
 };
@@ -49,10 +41,9 @@ export async function getStaticProps() {
   const news_posts = getNewsPosts();
   const academic_posts = getAcademicPosts();
   const life_posts = getLifePosts();
-  const interesting_tricks_posts = getInterestingTricksPosts();
 
   return {
-    props: { title: 'Posts', news_posts, academic_posts, life_posts, interesting_tricks_posts },
+    props: { title: 'Posts', news_posts, academic_posts, life_posts },
   };
 }
 
