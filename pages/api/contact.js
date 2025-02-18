76f3c8f615a69292e781a9d9b48e4732e7e48328
Chi Xing <chi.xing2025@gmail.com>
2025-02-18 13:09:58 +0000

    fix: contact form fail

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -5,6 +5,13 @@ const notion = new Client({
 });
 
 export default async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({ msg: 'Only POST requests are allowed' });
   }
