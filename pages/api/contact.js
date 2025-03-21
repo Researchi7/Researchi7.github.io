0673576a3afb7ca0c558f78f6f574850176bf833
Chi Xing <chi.xing2025@gmail.com>
2025-03-21 15:57:50 +0000

    correctly setting CROS head

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -8,6 +8,7 @@ export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'OPTIONS') {
     console.log("API Received Request:", req.body);
     return res.status(200).end();
